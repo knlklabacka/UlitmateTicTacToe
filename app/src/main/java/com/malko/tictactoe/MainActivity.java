3303cd89e9f96061c0564ba3c02ba90369f1e74e
Matt Klabacka <knlklabacka@gmail.com>
2015-06-12 12:41:41 -0500

    Initial commit

---
+++ app/src/main/java/com/malko/tictactoe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.malko.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
+}
